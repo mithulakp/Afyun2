45b9cf1347c43016eb85699d251a82f19ad57bc5
midhun kp <mithula@adamscreation.com>
2020-11-18 10:18:54 +0530

    added sections

---
+++ dist/js/custom.js
@@ -0,0 +1,23 @@
+const prevIcon = '<img src="dist/images/left.svg"class="prev-btn">Prev';
+const nextIcon = 'Next<img src="dist/images/right.svg"class="next-btn">';
+
+
+$('.banner-slide').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    pagination: false,
+    autoplay: 1000,
+    autoplaySpeed: 1000,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+})
