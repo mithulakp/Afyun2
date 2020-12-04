d731f083c8f936b0f3ee57be2d0927b96a63c295
midhun kp <mithula@adamscreation.com>
2020-12-04 20:12:31 +0530

    pages added

--- dist/js/custom.js
+++ dist/js/custom.js
@@ -1,6 +1,7 @@
 const prevIcon = '<img src="dist/images/slider-left-arw.svg"class="prev-btn slider-arrow">';
 const nextIcon = '<img src="dist/images/slider-right-arw.svg"class="next-btn slider-arrow">';
-
+const leftIcon = '<img src="dist/images/about-left-arw.svg"class="slider-arrow left-btn">';
+const rightIcon = '<img src="dist/images/about-right-arw.svg"class="slider-arrow right-btn">';
 
 $('.banner-slide').owlCarousel({
     loop: true,
@@ -21,6 +22,31 @@ $('.banner-slide').owlCarousel({
         },
         1000: {
             items: 1
+        }
+    }
+})
+
+
+$('.history-slide').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: false,
+    navText: [
+        leftIcon, rightIcon
+    ],
+    pagination: false,
+    autoplay: 1000,
+    autoplaySpeed: 1000,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 4
         }
     }
 })
